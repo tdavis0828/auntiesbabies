1af2381f52ee149583ccdd890dd596ddb35a645c
tdavis0828 <davis.t828@gmail.com>
2024-04-12 06:01:03 -0700

    about mobile styling

--- src/Styles/AboutStylesheet.jsx
+++ src/Styles/AboutStylesheet.jsx
@@ -97,7 +97,7 @@ export const StyledAbout = styled.main`
     }
   }
   @media (max-width: 425px) {
-    height: 425vh;
+    height: 400vh;
     & div {
       &.content-container {
         text-align: center;
